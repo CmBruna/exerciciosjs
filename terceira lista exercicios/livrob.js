4b86a9d7159887ffa89ca1b07eb7883e193c1a3c
Bruna <110406687+CmBruna@users.noreply.github.com>
2023-10-19 09:12:37 -0300

    exercicios

---
+++ terceira lista exercicios/livrob.js
@@ -0,0 +1,11 @@
+function imprimirLivro() {
+    const livro = {
+        titulo: "Os Sete Maridos de Evelyn Hugo",
+        autor: "Taylor Jenkins Reid",
+        numpag: 414
+    };
+    console.log("Título: " + livro.titulo);
+    console.log("Autor: " + livro.autor);
+    console.log("Número de Páginas: " + livro.numpag);
+}
+imprimirLivro();
