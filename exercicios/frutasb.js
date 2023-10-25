ee2336138ced759fc70c73e5c23595af19114ea5
Bruna <110406687+CmBruna@users.noreply.github.com>
2023-10-24 22:00:01 -0300

    exercicios

---
+++ exercicios/frutasb.js
@@ -0,0 +1,7 @@
+function imprimirFrutas() {
+    const frutas = ["Kiwi", "Banana", "Maça", "Uva"];
+    for (let i = 0; i < frutas.length; i++) {
+        console.log(frutas[i]);
+    }
+}
+imprimirFrutas();
---
+++ exercicios/nomeidadeb.js
@@ -0,0 +1,4 @@
+function imprimirNomeIdade (nome, idade) {
+    console.log(`Olá, meu nome é ${nome} e eu tenho ${idade} anos.`);
+}
+imprimirNomeIdade("Bruna", 18);
